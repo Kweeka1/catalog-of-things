2cee5bce8f336dac22b229c3fafa9196550ea936
belalgamal46 <belalgamal00@gmail.com>
2023-02-15 22:11:01 +0200

    Adjust game class and add to_object method

--- game.rb
+++ game.rb
@@ -2,16 +2,25 @@ require 'date'
 require_relative './item'
 
 class Game < Item
-  attr_accessor :last_played_at
+  attr_accessor :last_played_at, :name
 
-  def initialize(publish_date, last_played_at, multiplayer)
-    @publish_date = Date.parse(publish_date)
-    super(@publish_date)
-    @last_played_at = Date.parse(last_played_at)
+  def initialize(name, publish_date, last_played_at, multiplayer)
+    super(publish_date)
+    @last_played_at = last_played_at
     @multiplayer = multiplayer
+    @name = name
   end
 
   def can_be_archived?
     super && Date.today.year - @last_played_at.year > 2
+  end
+
+  def to_object
+    {
+      'name' => @name,
+      'publish_date' => @publish_date,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at,
+    }
   end
 end
